4ee1570ee7976481fcd3d881a3db7524666d1705
rasteli <gabrielrasteli3@gmail.com>
2021-12-26 21:30:47 -0300

    Include pywal colors.

--- config.def.h
+++ config.def.h
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
+//#include "/home/rasteli/.cache/wal/colors-wal-dmenu.h"
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -8,13 +9,13 @@ static const char *fonts[] = {
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#f8f8f2", "#282a36" },
-	[SchemeSel] = { "#f8f8f2", "#bd93f9" },
-	[SchemeOut] = { "#000000", "#44475a" },
+  //[>     fg         bg       <]
+  [SchemeNorm] = { "#f8f8f2", "#282a36" },
+  [SchemeSel] = { "#f8f8f2", "#bd93f9" },
+  [SchemeOut] = { "#000000", "#44475a" },
   [SchemeMid] = { "#eeeeee", "#44475a" },
- 	[SchemeSelHighlight] = { "#f1fa8c", "#bd93f9" },
-	[SchemeNormHighlight] = { "#f1fa8c", "#44475a" },
+  [SchemeSelHighlight] = { "#f1fa8c", "#bd93f9" },
+  [SchemeNormHighlight] = { "#f1fa8c", "#44475a" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
